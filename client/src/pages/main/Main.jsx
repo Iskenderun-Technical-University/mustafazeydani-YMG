a1289e4d1a38934ffad3891367dcb65af23381bc
mustafazeydani <mustafamari20@gmail.com>
2023-04-19 22:07:06 +0300

    Designed main page

--- client/src/pages/main/Main.jsx
+++ client/src/pages/main/Main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./main.css"
+import {Link} from "react-router-dom"
 
 function Main() {
   return (
     <div className='main'>
-        
+        <h1>GiveHope</h1>
+        <h2>Together, we can make a difference and provide hope for those in need - donate to our charity today!</h2>
+        <div className='buttons'>
+          <button className='btn'>Donate</button>
+          <button className='btn btn-main'>Request Aid</button>
+        </div>
+        <p>Check out the <Link className="donation-history" to="">donations history</Link></p>
     </div>
   )
 }
