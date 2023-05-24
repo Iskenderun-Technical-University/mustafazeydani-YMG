ffb7e167d1e80dd9dd23a9ebb7affad945083477
Waffa <mustafamari20@gmail.com>
2023-05-24 17:00:55 +0300

    Designed admin login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useNavigate} from "react-router-dom"
+import "../forms.css"
 
 function Login() {
+
   return (
-    <div>Login</div>
+    <div className='admin form'>
+      <form>
+        <h2>Admin Login</h2>
+        <input
+          name='username'
+          type="text" 
+          placeholder='username' 
+        />
+        <input
+          name='password'
+          on
+          type="password" 
+          placeholder='password' 
+        />
+        <p className='error'>Error</p>
+        <button className='btn btn-main'>Login</button>
+      </form>
+    </div>
   )
 }
 
