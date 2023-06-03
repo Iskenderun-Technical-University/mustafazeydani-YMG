c3954544fe338b2a3016cd57139878a1b982e485
Waffa <mustafamari20@gmail.com>
2023-06-03 10:14:18 +0300

    Designed donation request cards

--- client/src/pages/donationRequests/Dreq.jsx
+++ client/src/pages/donationRequests/Dreq.jsx
@@ -3,7 +3,18 @@ import "./dreq.css"
 
 function Dreq() {
   return (
-    <div>dreq</div>
+    <div className="dreq">
+      <h2>Donation Requests</h2>
+      <div className="dreq-container">
+        <div className="dreq-card">
+          <p className="dreq-name">Mustafa</p>
+          <p className="dreq-email">info@example.com</p>
+          <p className="dreq-number">+90 553 844 94 50</p>
+          {/* <p className="dreq-description">Hello</p> */}
+          <p className="dreq-amount">amout <span>1500$</span></p>
+        </div>
+      </div>
+    </div>
   )
 }
 
