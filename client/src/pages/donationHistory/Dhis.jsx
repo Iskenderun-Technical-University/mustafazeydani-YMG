84ef15ca89d285f57e975149ea5a5224d2c62268
Waffa <mustafamari20@gmail.com>
2023-06-04 20:49:39 +0300

    Added donation history table and donations fetching

--- client/src/pages/donationHistory/Dhis.jsx
+++ client/src/pages/donationHistory/Dhis.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import "./dhis.css"
+import axios from 'axios'
 
 function Dhis() {
+
+  const[donations, setDonations] = useState([])
+  const[fetching, setFetching] = useState(false)
+  const[error, setError] = useState(null)
+
+  // Fetching donations
+  useEffect(() => {
+    setFetching(true)
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("/donation")
+        setDonations(res.data)
+      } 
+      catch (err) {
+        setError("Error fetching donations")
+      }
+      setFetching(false)
+    }
+    fetchData()
+  }, [])
+
   return (
-    <div>dhis</div>
+    <div className="dhis">
+      <table>
+        <thead>
+          <tr>
+            <th>Donator's Name</th>
+            <th>Donater's Email</th>
+            <th>Donater's Number</th>
+            <th>Beneficiary's Name</th>
+            <th>Donation Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {donations.map((donation) => {
+            const {
+              uuid,
+              name,
+              email,
+              number,
+              beneficiary_name,
+              amount
+            } = donation;
+            return (
+              <tr
+                key={uuid}
+              >
+                <td>{name}</td>
+                <td className="donation-email">
+                    <a href={`mailto:${email}`}>{email}</a>
+                </td>
+                <td>
+                    <p>{number}</p>
+                </td>
+                <td>{beneficiary_name}</td>
+                <td>
+                    <p>{amount} TL</p>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
